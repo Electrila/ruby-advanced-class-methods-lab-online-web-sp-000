05d3b8c3a229567b62feef9bb7340927f86a3d01
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-23 01:47:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
   
   def self.new_from_filename(filename)
     filename = filename.to_s.split(" - ")
-    filename = filename.join(", ")
+    filename = filename.join(",")
     song = self.new
     self.artist_name = song[0]
     self.name = song[1].delete(".mp3")
