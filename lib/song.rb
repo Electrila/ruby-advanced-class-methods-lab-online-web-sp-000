bf881803676fc2663f786775148044ddfefae2a2
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-23 00:50:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def initialize(name, artist_name)
+    @name = name
+    @artist_name = artist_name
 
   def self.all
     @@all
